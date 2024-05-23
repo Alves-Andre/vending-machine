31aedf92a1844e8481708b393807d4caa320c45f
André Alves <andredasillvaa376@gmail.com>
2024-05-23 20:39:31 -0300

    teste

--- src/vendingmachine/App.java
+++ src/vendingmachine/App.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);        
